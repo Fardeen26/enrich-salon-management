c5ad6183e10af8f08ab77b0370247929f3f3196d
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-04 21:15:18 +0530

    changes in proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://b7v4hvw7-3000.inc1.devtunnels.ms/'
+      '/api': 'https://enrich-hair-salon.onrender.com'
     }
   },
   plugins: [react()],
