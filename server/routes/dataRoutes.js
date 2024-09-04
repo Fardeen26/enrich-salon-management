128d71b36f2ed3101f5d65f8bed5b89dec373f68
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-04 20:59:15 +0530

    modification for deployment

--- server/routes/dataRoutes.js
+++ server/routes/dataRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const dataController = require('../controllers/data')
 
 router.get("/service-data", dataController.serviceData);
-router.get('/testimonials', dataController.testimonialData)
 
 
 module.exports = router;
