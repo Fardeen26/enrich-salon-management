e18c0dfa513ce7a8ff72a1565663a2623e979941
Fardeen Mansuri <fardeenmansuri0316@gmail.com>
2024-09-05 20:31:05 +0530

    changes in mobile responciveness

--- client/src/components/Heading.jsx
+++ client/src/components/Heading.jsx
@@ -3,7 +3,7 @@ import MinimizeIcon from '@mui/icons-material/Minimize';
 // eslint-disable-next-line react/prop-types
 function Heading({ title }) {
     return (
-        <div className="heading px-44 text-center max-lg:px-28 max-md:px-20 text-3xl max-sm:px-4 max-sm:text-xl font-semibold">
+        <div className="heading px-44 text-center max-lg:px-28 max-md:px-20 text-3xl max-sm:px-4 max-sm:text-2xl font-semibold">
             <h2>{title}</h2>
             <span><MinimizeIcon style={{ marginTop: '-30px' }} /><MinimizeIcon style={{ marginTop: '-30px' }} /></span>
         </div>
--- client/src/components/Hero-Component/AboutSection.jsx
+++ client/src/components/Hero-Component/AboutSection.jsx
@@ -7,18 +7,18 @@ function AboutSection() {
   return (
     <div className='flex flex-col px-44 flex-wrap h-full py-16 max-lg:px-28 max-md:px-20 max-sm:px-4 max-sm:py-8 About' id='About'>
       <div className="desc-container w-full flex flex-wrap max-sm:justify-center">
-        <div className="desc-left flex-1 max-sm:text-center max-sm:text-3xl max-lg:text-4xl font-semibold"> WELCOME TO <br /> ENRICH HAIR SALOON
+        <div className="desc-left flex-1 max-sm:text-center max-sm:text-[27px] max-lg:text-4xl font-semibold max-[390px]:text-2xl"> WELCOME TO <br /> ENRICH HAIR SALOON
           <br />
           <span><MinimizeIcon /><MinimizeIcon /></span>
         </div>
         <div className="desc-right max-2xl:basis-1/2 max-xl:basis-1/2 max-md:basis-1/2 max-sm:basis-full max-sm:text-sm max-sm:px-4 max-sm:text-start max-sm:mt-3">Welcome to Enrich Salon, where beauty and luxury converge. Our expert stylists are committed to enhancing your unique style with personalized care. Whether it&apos;s a chic haircut, vibrant color, or a relaxing spa treatment, we ensure every visit leaves you refreshed and confident. Step into our modern space, and let us pamper you with top-notch service and premium products. Your beauty is our passion.
           <br />
-          <ScrollLink sx={{ my: 2, color: 'white', display: 'block' }} className='cursor-pointer' activeClass="active" to='Staff' spy={true} smooth={true} offset={-100} duration={600} ><button className="button mt-4">Our Stylists</button></ScrollLink>
+          <ScrollLink sx={{ my: 2, color: 'white', display: 'block' }} className='cursor-pointer' activeClass="active" to='Staff' spy={true} smooth={true} offset={-100} duration={600} ><button className="button mt-4 max-[390px]:p-2 max-[390px]:text-[12px]">Our Stylists</button></ScrollLink>
         </div>
       </div>
 
       <div className="look-container w-full flex justify-center mt-5">
-        <img src={Salon_Img} className='h-[500px] w-[900px]' />
+        <img src={Salon_Img} className='h-[500px] w-[900px] max-sm:h-[400px] max-[390px]:h-[350px]' />
       </div>
     </div>
   )
